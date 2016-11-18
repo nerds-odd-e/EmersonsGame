cf4771b76ee51ec186d300f4c6aed9e854c4adc0
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-19 01:40:54 +0800

    Try out WebDriver (which fails)

--- src/test/java/cucumber/stepdefs/HelloStepdefs.java
+++ src/test/java/cucumber/stepdefs/HelloStepdefs.java
@@ -1,17 +1,25 @@
 package cucumber.stepdefs;
 
 import cucumber.api.java8.En;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class HelloStepdefs implements En {
+    private WebDriver driver = new FirefoxDriver();
+
     public HelloStepdefs() {
         When("^I try to execute this feature$", () -> {
+            driver.get("http://localhost:8080/EmersonsGame");
         });
 
         Then("^I must see that it works$", () -> {
             assertThat(true, is(true));
+
+            driver.close();
+            driver.quit();;
         });
     }
 }
