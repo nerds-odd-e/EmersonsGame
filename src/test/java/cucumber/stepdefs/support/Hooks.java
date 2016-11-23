f00945a0c6e4fd3f4bf21d9f85c8e917046e3e9f
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-24 07:10:09 +0800

    Clear database on teardown

--- src/test/java/cucumber/stepdefs/support/Hooks.java
+++ src/test/java/cucumber/stepdefs/support/Hooks.java
@@ -3,18 +3,21 @@ package cucumber.stepdefs.support;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
+import static com.odde.emersonsgame.data.support.Databases.getDatabaseTester;
 import static cucumber.stepdefs.support.Url.url;
 import static cucumber.stepdefs.support.WebDriverFactory.getDriver;
 
 public class Hooks {
     @Before
-    public void beforeScenario() {
+    public void beforeScenario() throws Exception {
+        getDatabaseTester(true);
         getDriver(true).get(url());
     }
 
     @After
-    public void afterScenario() {
+    public void afterScenario() throws Exception {
         getDriver().close();
         getDriver().quit();
+        getDatabaseTester().onTearDown();
     }
 }
