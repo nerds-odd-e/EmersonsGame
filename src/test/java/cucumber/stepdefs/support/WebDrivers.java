cff9a65c947178e3c909b8c45eb5b6233967626d
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-24 11:39:27 +0800

    LOL no

--- src/test/java/cucumber/stepdefs/support/WebDrivers.java
+++ src/test/java/cucumber/stepdefs/support/WebDrivers.java
@@ -7,7 +7,9 @@ public class WebDrivers {
     private static WebDriver driver;
 
     public static WebDriver getNewDriver() {
-        return new FirefoxDriver();
+        driver = new FirefoxDriver();
+
+        return driver;
     }
 
     public static WebDriver getDriver() {
