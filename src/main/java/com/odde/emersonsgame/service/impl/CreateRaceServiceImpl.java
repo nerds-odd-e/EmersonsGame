f0b82d131bd1ae00b02e4420477bd0af23820071
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-25 00:02:25 +0800

    Actually create Race using RaceRepository

--- src/main/java/com/odde/emersonsgame/service/impl/CreateRaceServiceImpl.java
+++ src/main/java/com/odde/emersonsgame/service/impl/CreateRaceServiceImpl.java
@@ -1,11 +1,23 @@
 package com.odde.emersonsgame.service.impl;
 
+import com.odde.emersonsgame.data.RaceRepository;
+import com.odde.emersonsgame.data.impl.RaceRepositoryImpl;
 import com.odde.emersonsgame.model.Race;
 import com.odde.emersonsgame.service.CreateRaceService;
 
 public class CreateRaceServiceImpl implements CreateRaceService {
+    private RaceRepository raceRepository;
+
+    public CreateRaceServiceImpl() {
+        this(new RaceRepositoryImpl());
+    }
+
+    public CreateRaceServiceImpl(RaceRepository raceRepository) {
+        this.raceRepository = raceRepository;
+    }
+
     @Override
     public Race createRace(Race race) {
-        return race;
+        return raceRepository.create(race);
     }
 }
--- src/test/java/com/odde/emersonsgame/service/CreateRaceServiceTest.java
+++ src/test/java/com/odde/emersonsgame/service/CreateRaceServiceTest.java
@@ -1,27 +1,51 @@
 package com.odde.emersonsgame.service;
 
+import com.odde.emersonsgame.data.RaceRepository;
 import com.odde.emersonsgame.model.Race;
 import com.odde.emersonsgame.service.impl.CreateRaceServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class CreateRaceServiceTest {
     private CreateRaceService service;
 
+    @Mock
+    private RaceRepository raceRepository;
+
     @Before
     public void setUp() {
-        service = new CreateRaceServiceImpl();
+        MockitoAnnotations.initMocks(this);
+
+        service = new CreateRaceServiceImpl(raceRepository);
     }
 
     @Test
     public void CreateRaceMustSaveNewRace() {
+        when(raceRepository.create(any(Race.class))).thenReturn(new Race());
+
         Race race = new Race("New Race");
 
         Race savedRace = service.createRace(race);
 
         assertThat(savedRace, notNullValue());
+        assertThatRaceCreatedHasAttributes("New Race");
+    }
+
+    private void assertThatRaceCreatedHasAttributes(String name) {
+        ArgumentCaptor<Race> captor = ArgumentCaptor.forClass(Race.class);
+
+        verify(raceRepository).create(captor.capture());
+
+        assertThat(captor.getValue().getName(), is(name));
     }
 }
