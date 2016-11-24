f5521d9758a122612183b08548f94ccf679e268f
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-24 23:54:06 +0800

    No argument constructor

--- src/main/java/com/odde/emersonsgame/data/DataException.java
+++ src/main/java/com/odde/emersonsgame/data/DataException.java
@@ -3,6 +3,9 @@ package com.odde.emersonsgame.data;
 import java.sql.SQLException;
 
 public class DataException extends RuntimeException {
+    public DataException() {
+    }
+
     public DataException(SQLException e) {
         super(e);
     }
