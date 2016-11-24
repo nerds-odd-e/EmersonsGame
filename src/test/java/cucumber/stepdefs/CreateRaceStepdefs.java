17f426647f7cc11cf7b0740ed5a5603974b4fbf4
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-25 06:32:56 +0800

    Make this work

--- src/test/java/cucumber/stepdefs/CreateRaceStepdefs.java
+++ src/test/java/cucumber/stepdefs/CreateRaceStepdefs.java
@@ -1,16 +1,33 @@
 package cucumber.stepdefs;
 
-import cucumber.api.PendingException;
 import cucumber.api.java8.En;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static cucumber.stepdefs.support.Url.url;
+import static cucumber.stepdefs.support.WebDrivers.getDriver;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class CreateRaceStepdefs implements En {
     public CreateRaceStepdefs() {
         When("^I try to create a new race$", () -> {
-            throw new PendingException();
+            getDriver().get(url("/races/new"));
+
+            getDriver().findElement(By.id("name")).sendKeys("New Race");
+            getDriver().findElement(By.id("players")).sendKeys("4");
+
+            getDriver().findElement(By.id("submit")).click();
         });
 
         Then("^I must see the new race$", () -> {
-           throw new PendingException();
+            getDriver().get(url("/races"));
+
+            List<WebElement> rows = getDriver().findElements(By.cssSelector("#races tbody tr"));
+
+            assertThat(rows.size(), is(1));;
         });
     }
 }
