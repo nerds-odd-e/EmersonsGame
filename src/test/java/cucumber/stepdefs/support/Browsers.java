b957a097d22dfdbdb0f59cdfb1f5562cf5503d58
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-25 15:43:47 +0800

    Wait 1 second because Selenium

--- src/test/java/cucumber/stepdefs/support/Browsers.java
+++ src/test/java/cucumber/stepdefs/support/Browsers.java
@@ -3,12 +3,15 @@ package cucumber.stepdefs.support;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class Browsers {
     private static WebDriver driver;
 
     public static void openBrowser(final String url) {
         if (driver == null) {
             driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         }
 
         driver.get(url);
