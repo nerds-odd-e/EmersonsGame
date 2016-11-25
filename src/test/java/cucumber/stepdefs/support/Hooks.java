11c6ec8204e847498613351c982b94babfb8bddd
Ealden Esto E. Escañan <ealden@odd-e.com>
2016-11-25 15:12:51 +0800

    Do not open browser for all as we may not need it

--- src/test/java/cucumber/stepdefs/support/Hooks.java
+++ src/test/java/cucumber/stepdefs/support/Hooks.java
@@ -6,14 +6,11 @@ import cucumber.api.java.Before;
 import static com.odde.emersonsgame.data.support.Databases.getDatabaseTester;
 import static com.odde.emersonsgame.data.support.Databases.getNewDatabaseTester;
 import static cucumber.stepdefs.support.Browsers.closeBrowser;
-import static cucumber.stepdefs.support.Browsers.openBrowser;
-import static cucumber.stepdefs.support.Url.url;
 
 public class Hooks {
     @Before
     public void beforeScenario() throws Exception {
         getNewDatabaseTester();
-        openBrowser(url());
     }
 
     @After
